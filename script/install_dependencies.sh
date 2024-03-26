fe2a4fcad290a90bc28e54dbe19ceb3cd715da86
fjgibson <42211656+fj-gibson@users.noreply.github.com>
2024-03-26 17:50:44 -0600

    Update install_dependencies.sh

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -65,7 +65,7 @@ if [ ! -f "/usr/bin/aws" ]; then
     echo "---------------------------------------> Instal AWS Cli <---------------------------------------"
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     echo "---------------------------------------> Unzip AWS Cli <---------------------------------------"
-    unzip awscli-bundle.zip
+    unzip awscli-bundle.zip -A
     echo "---------------------------------------> Install AWS Cli <---------------------------------------"
     ./awscli-bundle/install -b /usr/bin/aws
 fi
